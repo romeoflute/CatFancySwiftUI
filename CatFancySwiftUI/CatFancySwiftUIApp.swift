9a970636cdd7a7ae7c4f27ab3c71999a4000f619
Romeo Flauta <meoflauta@gmail.com>
2024-03-15 21:47:05 +0800

    Initial Commit

---
+++ CatFancySwiftUI/CatFancySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFancySwiftUIApp.swift
+//  CatFancySwiftUI
+//
+//  Created by Romeo Flauta on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CatFancySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
