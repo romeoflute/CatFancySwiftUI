8606c37fcc41d9319a2438b495a2662a7691c9dc
Romeo Flauta <meoflauta@gmail.com>
2024-03-15 22:29:28 +0800

    Load breeds in BrowseBreedsView by using a viewmodel

--- CatFancySwiftUI/Views/BrowseBreedsView.swift
+++ CatFancySwiftUI/Views/BrowseBreedsView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct BrowseBreedsView: View {
+    var viewModel = BrowseBreedsViewModel()
     var body: some View {
-        Text("Hello, BrowseBreedsView!")
+        NavigationStack {
+            Group {
+                switch viewModel.state {
+                case .loading:
+                    ProgressView()
+                case .error:
+                    Text("An error occurred during breed fetching")
+                case .loaded(let breeds):
+                    if !breeds.isEmpty {
+                        Text("Loading succeeded. First breed: \(breeds.first?.name ?? "")")
+                    } else {
+                        Text("The endpoint returned an empty array of breeds.")
+                    }
+                }
+            }
+            .navigationTitle("Cat Breeds")
+        }
+        .task {
+            await viewModel.loadBreeds()
+        }
     }
 }
 
