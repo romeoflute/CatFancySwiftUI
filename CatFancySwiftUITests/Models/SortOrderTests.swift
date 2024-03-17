073ecb02cf8be707dc79f1fb719da017ff85f151
Romeo Flauta <meoflauta@gmail.com>
2024-03-17 20:59:01 +0800

    Add unit tests to SortOrder

---
+++ CatFancySwiftUITests/Models/SortOrderTests.swift
@@ -0,0 +1,30 @@
+//
+//  SortOrderTests.swift
+//  CatFancySwiftUITests
+//
+//  Created by Romeo Flauta on 3/17/24.
+//
+
+@testable import CatFancySwiftUI
+import XCTest
+
+class SortOrderTests: XCTestCase {
+    func testCompareTo() {
+        let a = "A"
+        let b = "B"
+        
+        let urlString = "https://racecondition.software"
+        guard let url = URL(string: urlString) else {
+            XCTFail(URL.couldNotInit(urlString: urlString))
+            return
+        }
+        
+        let breed1 = Breed(name: a, knownFor: "", popularity: 42, photoUrl: url, infoUrl: url, credit: "", license: .publicDomain, description: "")
+        let breed2 = Breed(name: b, knownFor: "", popularity: 21, photoUrl: url, infoUrl: url, credit: "", license: .publicDomain, description: "")
+        
+        var sortOrder = SortOrder.name
+        XCTAssert(sortOrder.compare(breed1: breed1, breed2: breed2))
+        sortOrder = .popularity
+        XCTAssertFalse(sortOrder.compare(breed1: breed1, breed2: breed2))
+    }
+}
