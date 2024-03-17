8978de72cb42af322b3700900088b30703b1b380
Romeo Flauta <meoflauta@gmail.com>
2024-03-17 20:22:33 +0800

    Add unit tests to ImageLoader

---
+++ CatFancySwiftUITests/Helpers/ImageLoaderTests.swift
@@ -0,0 +1,43 @@
+//
+//  ImageLoaderTests.swift
+//  CatFancySwiftUITests
+//
+//  Created by Romeo Flauta on 3/17/24.
+//
+
+@testable import CatFancySwiftUI
+import XCTest
+
+final class ImageLoaderTests: XCTestCase {
+    func testFetch() async throws {
+        guard let goodUrl = URL(string: MockData.photoUrlPrefix + MockData.breedNames[0] + MockData.mockPhotoExtension) else {
+            XCTFail("Unable to initialize good photo URL.")
+            return
+        }
+        
+        let imageLoader = ImageLoader()
+        var goodImage = await imageLoader.fetch(goodUrl)
+        XCTAssertNotNil(goodImage)
+        goodImage = await imageLoader.fetch(goodUrl)
+        XCTAssertNotNil(goodImage)
+        
+        let expectedErrorImage = UIImage(resource: .error)
+        
+        guard let badUrl = URL(string: "https://racecondition.software") else {
+            XCTFail("Unable to initialize bad photo URL.")
+            return
+        }
+        
+        let errorImage = await imageLoader.fetch(badUrl)
+        XCTAssertNotNil(errorImage)
+        
+        XCTAssertEqual(errorImage.pngData(), expectedErrorImage.pngData())
+    }
+    
+    func testSetSession() async {
+        Current.settings.sessionType = .shared
+        XCTAssertEqual(Current.settings.sessionType, .shared)
+        Current.settings.sessionType = .stub
+        XCTAssertEqual(Current.settings.sessionType, .stub)
+    }
+}
